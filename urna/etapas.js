4f27f27762bb0a7d15c0c959d6dd27014f99021e
David Alves <82853944+DavidDev01100100@users.noreply.github.com>
2022-08-08 19:09:14 -0300

    Add files via upload
    Commit inicial

---
+++ urna/etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55190',
+                nome: 'Gabriel',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'Gabriel.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '50123',
+                nome: 'Tarcisio',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'Tarcisio.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '25500',
+                nome: 'Cezar',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'cezar.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '10120',
+                nome: 'Carlos',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'carlos.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'ABC',
+                vice: 'Mourao',
+                fotos: [
+                    {url: 'Bolsonaro.jpg', legenda: 'Presidente'},
+                    {url: 'Mourao.jpg', legenda: 'Vice-Presidente', small: true}
+                ] 
+            },
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'DEFG',
+                vice: 'Manoela',
+                fotos: [
+                    {url: 'Lula.jpg', legenda: 'Presidente'},
+                    {url: 'Manoela.jpg', legenda: 'Vice-Presidente', small: true}
+                ] 
+            }
+        ]
+    }
+]
